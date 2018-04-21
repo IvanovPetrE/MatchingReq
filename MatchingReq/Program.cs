3a76a9d0e2a5bce2b2b08af9dffbf51f3dedf440
IvanovPetrE <ivanovpm7@gmail.com>
2018-04-21 23:38:27 +0500

    1

--- MatchingReq/Program.cs
+++ MatchingReq/Program.cs
@@ -77,30 +77,17 @@ namespace Matching
             while ((line = file.ReadLine()) != null) //В этом цикле получаем данные о заявках
             {
                 String[] vals = line.Split("\t");
+                // для каждой стоки из файла нужно проверить, есть ли в списке запрос, который удовлетворяет требованиям:
+                //продавец и покупатель разные лица, тип операции не совпадает, цена и количество совпадают.
+                //Если условия выполняются, то обрабатываемая строка в список не добавляется, а совпавшая строка из списка убирается.
+                //Если не выполняется, то обрабатываемая строка добавляется в список.
                 foreach (Request req in Requests)
                 {
                     if (Requests.Count != 0 & req.TName != vals[0] & req.reqType != vals[1] & req.psType == vals[2] & req.cost.ToString() == vals[3] & req.qt.ToString() == vals[4])
-                    //
                     {
-
-                        /*Console.WriteLine("");
-                        Console.WriteLine("");
-                        Console.WriteLine(line);
-                        Console.WriteLine(req.TName+"\t"+ req.reqType+ "\t" + req.psType+ "\t" + req.cost.ToString()+ "\t" + req.qt.ToString());
-                        Console.WriteLine("Req removed");
-                        Console.WriteLine("");
-                        Console.WriteLine("");
-
-                        Console.WriteLine("Old values");
-                        Console.WriteLine(Traders[vals[0]].getTraderData());
-                        Console.WriteLine(Traders[req.TName].getTraderData());*/
+                        //пересчитываем данные пользователей
                         Traders[vals[0]].buyOrSellPS(vals[1], vals[2], Int32.Parse(vals[3]), Int32.Parse(vals[4]));
                         Traders[req.TName].buyOrSellPS(req.reqType, req.psType, req.cost, req.qt);
-                        /*Console.WriteLine("New values");
-                        Console.WriteLine(Traders[vals[0]].getTraderData());
-                        Console.WriteLine(Traders[req.TName].getTraderData());
-                        Console.WriteLine("");
-                        Console.WriteLine("");*/
                         Requests.Remove(req);
                         condition = true; // помечаем, что текущую строку в очередь добавлять не нужно
                         break; //выходим из цикла foreach, иначе возникнет ошибка об изменении списка
@@ -114,10 +101,6 @@ namespace Matching
                 {
                     deltaRequest = new Request(vals[0], vals[1], vals[2], Int32.Parse(vals[3]), Int32.Parse(vals[4]));
                     Requests.Add(deltaRequest);
-                    /*Console.WriteLine(line + "\t" + counter.ToString());
-                    Console.WriteLine("Req added");
-                    Console.WriteLine("");
-                    condition = false;*/
                 }
                 counter++;
             }
@@ -140,7 +123,9 @@ namespace Matching
                 Console.ReadLine();
                 return;
             }
+            Console.WriteLine(@"Обработка данных завершена. Результаты выгружены в файл "+export);
             Console.ReadLine();
+            //
         }
     }
 }
---
+++ MatchingReqTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MatchingReqTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("USSC")]
+[assembly: AssemblyProduct("MatchingReqTests")]
+[assembly: AssemblyCopyright("Copyright © USSC 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8f895428-1b2c-4417-be72-3dc77f2ac626")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
